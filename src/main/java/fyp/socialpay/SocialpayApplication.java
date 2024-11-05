125063c35233f2ffebb45476a9c72e6d24558230
Matthew Siah <22010980@myrp.edu.sg>
2024-11-05 20:38:53 +0800

    push skeleton codes

---
+++ src/main/java/fyp/socialpay/SocialpayApplication.java
@@ -0,0 +1,13 @@
+package fyp.socialpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialpayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialpayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fyp/socialpay/SocialpayApplicationTests.java
@@ -0,0 +1,13 @@
+package fyp.socialpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialpayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
